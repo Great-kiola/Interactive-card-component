fe51c4afcf73b37aba8dfa06ac7160c86c37fdf0
Ola <hollabodey.kilaso@gmail.com>
2023-04-24 13:08:07 +0100

    Made changes to the html, css and js file

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 let confirm = document.querySelector('.confirm');
 let formdata = document.querySelector('#formData');
 let thankYou = document.querySelector('.thankYou');
-let continue = document.querySelector('.continue');
+let back = document.querySelector('.continue');
 
+
+let cardInput = document.querySelector('#cardHolder');
+let cardNumber = document.querySelector('#cardNumber');
+let cvcInput = document.querySelector('#cvc');
+
+let cardNo = document.querySelector('#number');
+let customerName = document.getElementById('name');
+let expiry = document.getElementById('exp');
+let cvc = document.querySelector('.cvc');
+
+
+cardInput.addEventListener("input", () => {
+    customerName.innerHTML = cardInput.value;
+})
+
+cardNumber.addEventListener("input", () => {
+    cardNo.innerHTML = cardNumber.value;
+});
+
+cvcInput.addEventListener("input", () => {
+    cvc.innerHTML = cvcInput.value;
+});
 
 
 confirm.addEventListener('click', () => {
     formdata.classList.add('visibility');
     thankYou.classList.remove('visibility');
 
+});
+
+back.addEventListener('click', () => {
+    formdata.classList.remove('visibility');
+    thankYou.classList.add('visibility');
 });
